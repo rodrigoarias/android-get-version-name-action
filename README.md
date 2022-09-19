0f396b4a437a2ae7279e854472cb141a99047a0a
rodrigoarias <rodrigo.arias@uala.com.ar>
2022-09-19 10:46:09 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # android-get-version-name-action
-Gets Android Version Name from build.gradle file
+Gets Android Version Name from build.gradle file.
+This is a simple and small action usefull for getting the current android version name without depending of gradle commands.
+For now it only works when dthe versionName is in the default gradle structure:
+```
+android {
+	defaultConfig {
+		versionName "1.0.0"
+	}
+}
+```
+
+## Inputs
+
+## `git-token`
+
+**Required** A GITHUB_SECRET with enough permissions to add a file.
+
+## `gradle-path`
+
+The path of the build.gradle file with the versionName. Default: `"app/build.gradle"`.
+
+## Outputs
+
+## `versionName`
+
+The Android app version name
+
+## Example usage
+
+uses: rodrigoarias/android-get-version-name-action@v1.0.0
+with:
+  git-token: ${{ secrets.GITHUB_TOKEN }}
---
+++ index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const g2js = require('gradle-to-js/lib/parser');
+
+const main = async (workspace) => {
+	const myToken = core.getInput('git-token');
+	const octokit = github.getOctokit(myToken);
+	const gradlePath = core.getInput('gradle-path');
+
+	const owner = github.context.payload.repository.owner.login
+	const repo = github.context.payload.repository.name
+
+	const manifest = await findFile(octokit, owner, repo, manifestPath);
+	const buff = Buffer.from(manifest.content, 'base64');
+  const content = buff.toString('ascii');
+
+  g2js.parseText(content).then(function(representation) {
+    var version = representation.android.defaultConfig.versionName;
+    console.log(`Version name detected: ${version}`);
+    core.setOutput("versionName", version);
+  });
+}
+
+const findFile = async(octo, organization, repo, fileName, branch) => {
+	console.log(`owner:${organization} repo:${repo} path:${fileName} ref:${branch}`)
+
+	gitFile = await octo.rest.repos.getContent({
+		owner: organization,
+		repo: repo,
+		path: fileName,
+	})
+	return gitFile;
+}
+
+main()
